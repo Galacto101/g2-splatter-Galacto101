448c39a6e6279270be387cdc48f408ed9b302004
Student <student@Admins-MacBook-Air.local>
2018-03-10 14:54:51 -0500

    update

--- script.js
+++ script.js
@@ -1,30 +1,47 @@
-// declare and initialize global variables
+// Declare and initialize global variables
+var canvas;
+var clearButton;
+var spread;
+var slider;
 
-/*
- * setup()
- */
+function setup() {
+  canvas = createCanvas(500, 400);
+  canvas.parent("#canvas-area");
+  canvas.mousePressed(drawSplatter);
+  clearButton = select("#clear-button");
+  clearButton.mousePressed(clearScreen);
+  slider = select("#slider");
+  slider.input(updateSpread);
+  background(0);
+  //spread = 50;
+}
 
+function updateSpread(){
+  spread = slider.value();
+}
 
-/*
- * drawEllipse()
- */
+function drawEllipse() {
+  fill("#02ffff");
+  noStroke();
+  ellipse(mouseX, mouseY, 100, 100);
+}
 
+function clearScreen() {
+  background(0);
+}
 
-/*
- * updateSpread()
- * This function simply sets the spread variable to the value of the slider.
- * For example, if the slider is at 75, the spread will be set to 75 as well.
- */
+function drawSplatter() {
+  var randomNum = random(10, 15);
 
+    for(var i = 0; i < randomNum; i++){
+      var r = random(256);
+      var g = 221;
+      var b = 221;
+      fill(r,g,b);
+      var randomSize = random(5, 15);
+      var x = random(mouseX - spread, mouseX + spread);
+      var y = random(mouseY - spread, mouseY + spread);
+      ellipse(x, y, randomSize, randomSize);
+    }
 
- /*
-  * drawSplatter()
-  * Creates ellipses on the canvas around where the mouse is clicked.
-  * Details:
-  * -The function creates between 10 and 14 ellipses (random).
-  * -Each ellipse will be a random color and have no outline.
-  * -Each ellipse will have a random radius (5 to 14 pixels).
-  * -Each ellipse's x and y values are calculated like this:
-  *  x = random value between mouseX - spread and mouseX + spread
-  *  y = random value between mouseY - spread and mouseY + spread
-  */
+}
